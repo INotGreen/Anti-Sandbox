8de9c757a97790d3a3aa2cd4894819c50f78f0a7
Nicholas Frischkorn <45154327+NickFrischkorn@users.noreply.github.com>
2021-10-06 21:17:25 -0400

    Create sleep.cs

---
+++ sleep.cs
@@ -0,0 +1,20 @@
+namespace Sleep
+{
+    public class Program
+    {      
+        [DllImport("kernel32.dll")]
+        static extern void Sleep(uint dwMilliseconds);
+		
+	public static void Main(string[] args)
+        {
+            DateTime t1 = DateTime.Now;
+            Sleep(8000);
+            double t2 = DateTime.Now.Subtract(t1).TotalSeconds;
+            if (t2 < 7.0)
+            {
+                return;
+            }
+        }
+		
+    }
+}
